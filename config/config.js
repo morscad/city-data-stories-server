29d27515bde52191af5cc3cbe9711ebf755f674c
Omar Faleh <omar@therefore.ca>
2020-04-15 11:42:16 -0400

    use environment variables for db credentials

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  host: process.env.DB_HOST || process.env.CLEARDB_DATABASE_URL || "localhost",
-  user: process.env.DB_USER || process.env.CLEARDB_DATABASE_USER || "root",
-  password: process.env.DB_PASS ||process.env.CLEARDB_DATABASE_PASS ||  "root",
-  database: process.env.DB_NAME || process.env.CLEARDB_DATABASE_NAME || "myDB"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 }
